55f6e8bcf8a1e26cded298ba21d6ede5f74e097a
krishna <kp1406055@gmail.com>
2023-08-26 10:22:26 +0530

    Updated calculator_brain.dart

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,52 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'dart:math';
-
-// class CalculatorBrain{
-
-//   int height;
-//   int weight;
-//   // double bmi=20;
-//   double bmi; 
-//   String _bmi='';
-
-//   CalculatorBrain(
-//     this.bmi,
-//     this.height,
-//     this.weight,
-//   );
-
-//   String GetResult()
-//   {
-//     this.bmi = weight/pow(height, 2);
-//     this._bmi = bmi.toStringAsFixed(2);
-//     return _bmi;
-//   }
-
-//   String GetDescription(){
-//     if (this.bmi > 25){
-//       return 'Overweight';
-//     }
-//     else if(this.bmi < 18){
-//       return 'Underweight';
-//     }
-//     else{
-//       return 'Normal';
-//     }
-//   }
-
-//   String GetInterpretation(){
-//     if (this.bmi > 25){
-//       return 'You need to exercise';
-//     }
-//     else if(this.bmi < 18){
-//       return 'You need to eat more';
-//     }
-//     else{
-//       return 'You are normal...you just need to chill...euuuuuuu';
-//     }
-//   }
-// }
-
 import 'dart:math';
 
 class CalculatorBrain{
@@ -65,23 +16,23 @@ class CalculatorBrain{
     if(this._bmi >= 25.0){
       return 'Overweight';
     }
-    else if(this._bmi >= 18.5){
-      return 'Normal';
+    else if(this._bmi <= 18.5){
+      return 'Underweight';
     }
     else{
-      return 'Underweight';
+      return 'Normal';
     }
   }
 
   String GetInterpretation(){
     if(_bmi >= 25){
-      return 'You have a higher than normal body weight. Try exercising more!';
+      return 'You have a higher than Normal body weight, try exercising more';
     }
-    else if(_bmi >= 18.5){
-      return 'You have a normal body weight. GOOD JOB!!';
+    else if(_bmi <= 18.5){
+      return 'You have a lesser than Normal body weight, try to eat more!';
     }
     else{
-      return 'You have a lesser than normal body weight. Eat more food!';
+      return 'Congratulations! , You have a normal body weight!';
     }
   }
 }
